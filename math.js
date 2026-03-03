1f33aca176a68622fff4a169326fba82a9029943
aalmeidaa <140685917+aalmeidaa@users.noreply.github.com>
2026-03-03 00:28:37 +0000

    Teste

---
+++ math.js
@@ -0,0 +1,29 @@
+// add(a, b): Retorna a soma de a e b.
+// subtract(a, b): Retorna a diferença entre a e b.
+// multiply(a, b): Retorna o produto de a e b.
+// divide(a, b): Retorna a divisão de a por b.
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+console.log(add( 10, 5))
+
+module.exports = {
+    add,
+    divide,
+    multiply,
+    subtract
+}
