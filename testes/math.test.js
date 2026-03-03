58e1695df877b69c34dff478db0094a3e483bfab
aalmeidaa <140685917+aalmeidaa@users.noreply.github.com>
2026-03-03 00:49:00 +0000

    Testando as demais operações

--- testes/math.test.js
+++ testes/math.test.js
@@ -3,3 +3,15 @@ const math=require('../math');
 test('soma 2 + 3 igual a 5', () => { 
 expect(math.add(2, 3)).toBe(5); 
 }); 
+
+test('subtração 5 - 3 igual a 2', () => { 
+expect(math.subtract(5, 3)).toBe(2); 
+}); 
+
+test('multiplicação 2 * 3 igual a 6', () => { 
+expect(math.add(2, 3)).toBe(6); 
+}); 
+
+test('divisão 4 / 2 igual a 2', () => { 
+expect(math.add(2, 3)).toBe(2); 
+}); 
